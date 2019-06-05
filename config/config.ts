1904e0e2e3a6b31957ea4f38c1a389731d9f2369
my-reacter <yaoqqiulin@gmail.com>
2019-06-05 11:14:10 +0800

    实例blank区块

--- config/config.ts
+++ config/config.ts
@@ -4,9 +4,7 @@ import slash from 'slash2';
 import { IPlugin, IConfig } from 'umi-types';
 import defaultSettings from './defaultSettings';
 import webpackPlugin from './plugin.config';
-const { pwa, primaryColor } = defaultSettings;
-
-// preview.pro.ant.design only do not use in your production ;
+const { pwa, primaryColor } = defaultSettings; // preview.pro.ant.design only do not use in your production ;
 // preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
 
 const { ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION, TEST, NODE_ENV } = process.env;
@@ -112,6 +110,11 @@ export default {
           icon: 'smile',
           component: './Welcome',
         },
+        {
+          name: 'blank',
+          path: '/blank',
+          component: './blank',
+        },
       ],
     },
   ],
@@ -139,7 +142,7 @@ export default {
         resourcePath: string;
       },
       localIdentName: string,
-      localName: string,
+      localName: string
     ) => {
       if (
         context.resourcePath.includes('node_modules') ||
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 
 export default () => (
   <p style={{ textAlign: 'center' }}>
-    Want to add more pages? Please refer to{' '}
-    <a href="https://umijs.org/guide/block.html" target="_blank" rel="noopener noreferrer">
-      umi block
-    </a>
-    。
+    Want to add more pages? {'🤮 '}
   </p>
 );
